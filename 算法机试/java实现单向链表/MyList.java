773add554669617bc7988fa5d3c01107749f7476
15091329896 <2200364978@qq.com>
2019-12-17 17:13:39 +0800

    链表

---
+++ 算法机试/java实现单向链表/MyList.java
@@ -0,0 +1,110 @@
+package com.test.mybatis.test;
+
+public class MyList {
+	private int size;
+	private ListNode firstNode;
+
+	public int getLongestSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public ListNode getFirstNode() {
+		return firstNode;
+	}
+
+	public void setFirstNode(ListNode firstNode) {
+		this.firstNode = firstNode;
+	}
+
+	public MyList(int size, ListNode firstNode) {
+		super();
+		this.size = size;
+		this.firstNode = firstNode;
+	}
+
+	public ListNode add(ListNode listNode) {
+		if(firstNode == null) {
+			this.firstNode = listNode;
+			return listNode;
+		}
+		int currentSize = getRealSize();
+		if(currentSize >= size) {
+			return null;
+		}
+		ListNode next = this.firstNode;
+		while (next.getNext()!= null) {
+			next = next.getNext();
+		}
+		next.setNext(listNode);
+		return listNode;
+	}
+
+	public int getRealSize() {
+		if(this.firstNode == null) {
+			return 0;
+		}
+		int currentSize = 1;
+		ListNode next = this.firstNode;
+		while (next.getNext() != null) {
+			next = next.getNext();
+			currentSize++;
+		}
+		return currentSize;
+	}
+
+	public ListNode delete(int index) {
+		ListNode deleteNode = null;
+		while(true) {
+			if(index == 0) {
+				deleteNode = this.firstNode;
+				this.firstNode = firstNode.getNext();
+				break;
+			}
+			ListNode next = this.firstNode.getNext();
+			int i = 1;
+			i++;
+			if(index == i) {
+				deleteNode = next.getNext();
+				next.setNext(next.getNext().getNext());
+				break;
+			} else {
+				next = next.getNext();
+			}
+		}
+		return deleteNode;
+	}
+	
+	
+	public ListNode get(int index) {
+		
+		if(index == 0) {
+			return this.firstNode;
+		}
+		ListNode next = this.firstNode.getNext();
+		int i = 1;
+		while(index != i) {
+			next = next.getNext();
+			i++;
+		}
+		return next;
+	}
+	
+	public boolean clean() {
+		this.firstNode = null;
+		return true;
+		
+	}
+	
+	public void getAll() {
+		for (int i = 0; i < this.getRealSize(); i++) {
+			System.out.println(this.get(i));
+		}
+		
+	}
+	
+	
+}
